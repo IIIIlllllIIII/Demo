50f9bd02500f408280bb61f93f44f1b008c78e10
IIIIlllllIIII <45705839+IIIIlllllIIII@users.noreply.github.com>
2024-04-03 12:58:07 +0800

    2024/4/3

--- Demo59_SQLite.py
+++ Demo59_SQLite.py
@@ -1,29 +1,14 @@
-#SQLite
-# 导入SQLite
+#SQLite数据库,SQLite占位符是"?"
 import sqlite3
-# 连接到SQLite数据库
-# 数据库文件是test.db
-# 如果文件不存在，会自动在当前目录创建:
-conn = sqlite3.connect('test.db')
-# 创建一个Cursor:
-cursor = conn.cursor()
-# 执行一条SQL语句，创建user表:
-cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
 
-# 继续执行一条SQL语句，插入一条记录:
-cursor.execute('insert into user (id, name) values (\'1\', \'Michael\')')
-
-# 通过rowcount获得插入的行数:
-cursor.rowcount
-
-# 提交事务:
-conn.commit()
-# 关闭Cursor:
-cursor.close()
-# 关闭Connection:
-conn.close()
-
-import sqlite3
 conn = sqlite3.connect('test.db') #实例化连接对象
 cursor = conn.cursor()    #实例化游标对象
-cursor.execute()
+#创建user表
+#cursor.execute('create table user (id varchar(20) primary key , name varchar(20))')    #execute执行
+cursor.execute('insert into user (id, name) values (\'3\', \'Jerry\')')   #插入一条数据('2', 'Bob')
+row_count = cursor.rowcount  #返回插入的行数,这里的rowcount是一个属性表示行数
+print(row_count) #打印插入的行数
+
+conn.commit()   #提交事务
+cursor.close()  #关闭游标
+conn.close()    #关闭连接
