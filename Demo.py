5abf800260417fcdebf0f46abede01fa71c795f0
IIIIlllllIIII <45705839+IIIIlllllIIII@users.noreply.github.com>
2025-08-14 14:39:37 +0800

    refactor(vscode): 更新 Visual Studio 2022 路径并修改示例代码
    - 更新 .vscode/settings.json 中的 Visual Studio 路径
    - 从 VR_NR 改为 2022
    - 修改 Demo.py 中的示例代码
    - 删除原有的计算器功能
    - 添加新的函数 make_funcs 和 primes
    - 演示闭包和生成器的使用

--- Demo.py
+++ Demo.py
@@ -1,24 +1,57 @@
 import sys
 import os
+import math
 
+import psutil
+from collections.abc import Iterable
 from functools import reduce
 
-def str2num(s):
-    try :
-        return int(s)
-    except ValueError as a:
-        print('ValueError:', a) 
-        return float(s)
 
-def calc(exp):
-    ss = exp.split('+')
-    ns = map(str2num, ss)
-    return reduce(lambda acc, x: acc + x, ns)
 
+  
+def make_funcs():
+    funcs = []
+    x = 0
+    for i in range(3):
+        def f():
+            nonlocal x
+            x = x + 1
+            return x + i
+        funcs.append(f)
+    return funcs
+
+f1, f2, f3 = make_funcs()
+print(f1())  # 1
+print(f2())  # 2
+print(f3())  # 3
+print(f1())  # 4
+
+
+
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+def _not_divisible(n):
+    return lambda x: x % n > 0
+
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n), it)
+
+L = list(filter(lambda x: x % 2 == 1, range(1, 20)))
+print(L)
 def main():
-    r = calc('100 + 200 + 345')
-    print('100 + 200 + 345 =', r)
-    r = calc('99 + 88 + 7.6')
-    print('99 + 88 + 7.6 =', r)
-
+    for n in primes():
+        if n < 100:
+            print(n)
+        else:
+            break
+        
 main()
